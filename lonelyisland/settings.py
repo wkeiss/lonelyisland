0b74fdc995422a51630ce1af68583d1143610b15
Cindy <1593006160@qq.com>
2020-11-30 17:32:37 +0800

    update

--- lonelyisland/settings.py
+++ lonelyisland/settings.py
@@ -134,3 +134,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 LOGIN_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
